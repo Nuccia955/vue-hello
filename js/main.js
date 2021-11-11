f1edf419d2504b0483dda9e3b04f2b823b9781ad
Nuccia955 <elenamunaro1995@gmail.com>
2021-11-11 14:13:55 +0100

    Hello Vue completed

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//VueJS
+const app = new Vue (
+    {
+        el: '#app',
+        data: {
+            message: 'Hello VueJs!',
+            image: 'https://dwglogo.com/wp-content/uploads/2017/09/Vue-logo-001.svg',
+        }
+    });
